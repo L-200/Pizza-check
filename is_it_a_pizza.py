5ef9b17e636a6fab38d68add4ea862615ede57c5
L-200 <83735407+L-200@users.noreply.github.com>
2024-04-20 17:44:40 -0400

    Update is_it_a_pizza.py
    A small optimization of the original code

--- is_it_a_pizza.py
+++ is_it_a_pizza.py
@@ -1,10 +1,11 @@
 print("Hello, this wil help you indentify wether your food is a pizza")
 print("Are you eating a pizza?(please say yes or no)")
 awnser = input() # user input 
-if awnser == "yes" or awnser =="Yes" or awnser =="YES":  #check if awnser says yes
+awnser = awnser.lower()
+if awnser == "yes":  #check if awnser says yes
     print("You are eating a pizza, congratulations :)")
 
-elif awnser == "no" or awnser=="No" or awnser =="NO": #check if awnser says no
+elif awnser == "no": #check if awnser says no
     print("You are not eating a pizza, sorry :(")
 
 else:
